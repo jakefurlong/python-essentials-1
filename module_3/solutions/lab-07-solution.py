d077092882b37f9f49f308526e85187310edab58
jakefurlong <jake.w.furlong@gmail.com>
2023-12-23 06:51:36 -0700

    added solution for lab 7

---
+++ module_3/solutions/lab-07-solution.py
@@ -0,0 +1,14 @@
+# Code
+
+def break_statements():
+    while True:
+        response = input("Enter a word: ")
+        if response == 'chubacabra':
+            print('You\'ve successfully left the loop.')
+            break
+
+break_statements()
+
+# Driver code
+
+# No tests for this lab
