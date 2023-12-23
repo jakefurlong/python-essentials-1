d9ce0f83510b35188d477e810c351e806f674697
jakefurlong <jake.w.furlong@gmail.com>
2023-12-22 18:33:55 -0700

    added driver code unit tests for lab 2

--- module_3/solutions/lab-02-solution.py
+++ module_3/solutions/lab-02-solution.py
@@ -8,7 +8,7 @@ def is_spathiphyllum(user_string):
     if user_string == 'Spathiphyllum':
         return 'Yes - Spathiphyllum is the best plant ever!'
     elif user_string == 'spathiphyllum':
-        return 'No, I want a big Spathiphyllum!'
+        return 'No, I want a big Spathiphyllum'
     else:
         return f'Spathiphyllum! Not {user_string}!'
     
@@ -16,4 +16,39 @@ print(is_spathiphyllum(user_string))
 
 # Driver code
 
-# Create driver code here
+print('\nRunning units tests...')
+
+submit_cases = [
+    ('spathiphyllum', 'No, I want a big Spathiphyllum'),
+    ('pelargonium', f'Spathiphyllum! Not pelargonium!'),
+    ('Spathiphyllum','Yes - Spathiphyllum is the best plant ever!')
+]
+
+def test(input, expected_output):
+    print("---------------------------------")
+    print(f"Input: {input}")
+    print(f"Expecting: {expected_output}")
+    result = is_spathiphyllum(input)
+    print(f"Actual: {result}")
+    if result == expected_output:
+        print("Pass")
+        return True
+    print("Fail")
+    return False
+
+def main():
+    passed = 0
+    failed = 0
+    for test_case in submit_cases:
+        correct = test(*test_case)
+        if correct:
+            passed += 1
+        else:
+            failed += 1
+    if failed == 0:
+        print("============= PASS ==============")
+    else:
+        print("============= FAIL ==============")
+    print(f"{passed} passed, {failed} failed")
+
+main()
