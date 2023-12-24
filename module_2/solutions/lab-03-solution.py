d8cfbe8e654d22e388ccbfd77c4f724fb6cf94ef
jfurlong <jfurlong@jbsinternational.com>
2023-12-24 13:05:14 -0700

    added solution and instructions for lab 3

--- module_2/solutions/lab-03-solution.py
+++ module_2/solutions/lab-03-solution.py
@@ -1,1 +1,17 @@
 # 2.1.1.19
+
+# for the first step, I put everything in a single line of code
+print("    *", "   * *", "  *   *", " *     *", "***   ***", "  *   *", "  *   *", "  *****",sep='\n')
+
+# for the second step, I had to double the single asterices because of the unequal ratios
+print("         **", "      **    **", "    **        **", "  **            **", "******        ******", "     *        *", "     *        *", "     **********", sep='\n\n')
+
+# for the third step, I just multiplied the original by 2 and adjusted the spacing on the right
+print("    *    "* 2) 
+print("   * *   "* 2)
+print("  *   *  "* 2)
+print(" *     * "* 2)
+print("***   ***"* 2)
+print("  *   *  "* 2)
+print("  *   *  "* 2)
+print("  *****  "* 2)
