1d5590ed4c765b254a426e9ddfbc6780b5ac87d0
jakefurlong <jake.w.furlong@gmail.com>
2023-12-27 16:06:14 -0700

    added solution and instructions for lab 8

--- module_2/solutions/lab-08-solution.py
+++ module_2/solutions/lab-08-solution.py
@@ -1,1 +1,14 @@
 # 2.5.1.2
+
+# this program computes the number of seconds in a given number of hours
+# 12/24/2023 by jfurlong
+
+a = 2 # number of hours
+seconds = 3600 # number of seconds in 1 hour
+ 
+print(f"Seconds in {a} Hours:", a * seconds)
+
+a = 3 # try 3 hours - expected output = 10800
+print(f"Seconds in {a} Hours:", a * seconds)
+
+print("Goodbye")
