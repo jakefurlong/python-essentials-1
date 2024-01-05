080f5bd275f4d332e165da9ae17609b9968880b3
jfurlong <jfurlong@jbsinternational.com>
2024-01-04 18:22:29 -0700

    added solution and instructions for lab 11

--- module_2/solutions/lab-11-solution.py
+++ module_2/solutions/lab-11-solution.py
@@ -1,1 +1,16 @@
 # 2.6.1.11
+
+hour = int(input("Starting time (hours): "))
+mins = int(input("Starting time (minutes): "))
+dura = int(input("Event duration (minutes): "))
+
+# Write your code here.
+
+# this assumes that we aren't rolling time from 23:59 into a new day
+
+total_time_in_minutes = (hour * 60) + mins + dura
+
+hours = total_time_in_minutes // 60
+minutes = total_time_in_minutes % 60
+
+print(f"{hours}:{minutes}")
